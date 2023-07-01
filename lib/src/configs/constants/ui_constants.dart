e971076e855a528151374421e1bb8b2110c1be4c
Nestor cruz <nestor.cruzq@hotmail.com>
2023-07-01 15:02:21 -0500

    path asset images fixed

--- lib/src/configs/constants/ui_constants.dart
+++ lib/src/configs/constants/ui_constants.dart
@@ -1,10 +1,8 @@
 // ignore_for_file: constant_identifier_names
-import 'package:flutter/foundation.dart';
 
 class UIConstants {
-  static const String base = kDebugMode ? '' : 'assets/';
-  static const String LOGO = '${base}images/pokemon_logo_small.png';
-  static const String SPACE_BACKGROUND = '${base}images/backgrounds/space_background.jpg';
-  static const String POKEMON_TYPE = '${base}images/types';
-  static const String POKEMON_GENERATION = '${base}images/generations';
+  static const String LOGO = 'images/pokemon_logo_small.png';
+  static const String SPACE_BACKGROUND = 'images/backgrounds/space_background.jpg';
+  static const String POKEMON_TYPE = 'images/types';
+  static const String POKEMON_GENERATION = 'images/generations';
 }
